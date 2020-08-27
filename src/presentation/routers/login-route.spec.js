26b46035215b12c025154fa4e78d6cd2ac8fff1d
Matheus.Oliveira <matheus.oliveira@sillion.com.br>
2020-08-26 23:45:31 -0300

    ensure LoginRouter returns 500 if an invalid EmailValidator is provided

--- src/presentation/routers/login-route.spec.js
+++ src/presentation/routers/login-route.spec.js
@@ -180,4 +180,32 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(400) // Compare the pointer between two objects
     expect(httpResponse.body).toEqual(new InvalidParamError('email')) // Compare the value between two objects
   })
+
+  test('Should return 500 if no EmailValidator is provided', async () => {
+    const authUseCaseSpy = makeAuthUseCase()
+    const sut = new LoginRouter(authUseCaseSpy)
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError()) // Compare the value between two objects
+  })
+
+  test('Should return 500 if EmailValidator has no isValid method', async () => {
+    const authUseCaseSpy = makeAuthUseCase()
+    const sut = new LoginRouter(authUseCaseSpy, {})
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError()) // Compare the value between two objects
+  })
 })
